a0dfc5005a4985ad04f319291ba4e276519e153c
mohsenXAD <mohsenxad@gmail.com>
2022-01-03 14:27:19 +0330

    close all details on click over

--- src/app/componets/certification/action-panel/certification-action-panel.component.ts
+++ src/app/componets/certification/action-panel/certification-action-panel.component.ts
@@ -43,11 +43,23 @@ export class CertificationActionPanelComponent implements OnInit {
   close(){
     console.log('closing');
     
-    const details = document.getElementById("certification-action-panel-details");
-    console.log(details);
-    console.log(details.id);
+    // const details = document.getElementById("certification-action-panel-details");
+    // console.log(details);
+    // console.log(details.id);
     
-    details.removeAttribute("open");
+    // details.removeAttribute("open");
+
+    const details = document.querySelectorAll("details");
+
+    // Add the onclick listeners.
+    details.forEach((targetDetail) => {
+      targetDetail.addEventListener("click", () => {
+        // Close all the details that are not targetDetail.
+        details.forEach((detail) => {
+          detail.removeAttribute("open");
+        });
+      });
+    });
   }
 
 }
