68fb3f5038173f66166d41e57f2c91d48b6c5db4
mohsenXAD <mohsenxad@gmail.com>
2022-02-26 18:34:42 +0330

    add/ select certificaiton in add project

--- src/app/componets/assignedCertification/select-or-add/select-or-add-assigned-certification.component.ts
+++ src/app/componets/assignedCertification/select-or-add/select-or-add-assigned-certification.component.ts
@@ -9,7 +9,13 @@ export class SelectOrAddAssignedCertificationComponent implements OnInit {
 
   constructor() { }
 
+  actionType:String = 'Existing';
+
   ngOnInit(): void {
+  }
+
+  changed(actionType: String):void{
+    this.actionType = actionType
   }
 
 }
